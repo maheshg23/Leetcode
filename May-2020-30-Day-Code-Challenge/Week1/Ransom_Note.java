4650fd65f1412a6d0d122584cdc72691c4ad7f21
maheshg23 <gmn1994@gmail.com>
2020-05-22 19:41:36 -0400

    Add Ransom Note Sol

--- May-2020-30-Day-Code-Challenge/Week1/Ransom_Note.java
+++ May-2020-30-Day-Code-Challenge/Week1/Ransom_Note.java
@@ -1,2 +1,19 @@
 // https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3318/
 // No. 383 https://leetcode.com/problems/ransom-note/
+
+class Solution {
+    public boolean canConstruct(String ransomNote, String magazine) {
+        
+        int[] letters = new int[26];
+        for(char c : magazine.toCharArray()){
+            letters[ c - 'a'] = letters[c - 'a'] + 1;
+        }
+        
+        for(char c : ransomNote.toCharArray()){
+            if(letters[c-'a'] == 0)
+                return false;
+            letters[c-'a']--;
+        }
+        return true;
+    }
+}
