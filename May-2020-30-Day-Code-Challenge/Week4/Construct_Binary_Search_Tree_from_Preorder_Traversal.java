4a162c295e14009f7b72320c5c08687df494d3e3
maheshg23 <gmn1994@gmail.com>
2020-05-26 12:22:21 -0400

    Add May Week4 Day24

---
+++ May-2020-30-Day-Code-Challenge/Week4/Construct_Binary_Search_Tree_from_Preorder_Traversal.java
@@ -0,0 +1,38 @@
+// https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/537/week-4-may-22nd-may-28th/3339/
+// No. 1008 https://leetcode.com/problems/construct-binary-search-tree-from-preorder-traversal/
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode bstFromPreorder(int[] preorder) {
+        
+        TreeNode root = null;
+        for(int num : preorder){
+            root = buildBST(root, num);
+        }
+    return root;
+    }
+    
+    public TreeNode buildBST(TreeNode node, int x){
+        if(node == null)
+            return node = new TreeNode(x);
+        if(x < node.val)
+            node.left = buildBST(node.left, x);
+        else if(x > node.val)
+            node.right = buildBST(node.right, x);
+        return node;
+    }
+}
