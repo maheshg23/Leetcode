52545b84906a62eed3b8f048be3b5ef23714557b
maheshg23 <gmn1994@gmail.com>
2020-05-29 12:23:13 -0400

    Add May Week4 Day19

---
+++ May-2020-30-Day-Code-Challenge/Week3/Online_Stock_Span.java
@@ -0,0 +1,28 @@
+// https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/536/week-3-may-15th-may-21st/3334/
+// No. 901 https://leetcode.com/problems/online-stock-span/
+
+class StockSpanner {
+    
+    Stack<int[]> st;
+    
+    public StockSpanner() {
+        st = new Stack<>(); 
+    }
+    
+    public int next(int price) {
+        
+        int res = 1;
+        while(!st.isEmpty() && st.peek()[1] <= price){
+            res += st.pop()[0] ;
+        }
+        
+        st.add(new int[]{res, price});
+        return res;
+    }
+}
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner obj = new StockSpanner();
+ * int param_1 = obj.next(price);
+ */
