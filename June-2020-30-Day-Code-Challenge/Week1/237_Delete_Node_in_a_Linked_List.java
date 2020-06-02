8473c9ff796fd967e6642b13140a5f716f6512f8
maheshg23 <gmn1994@gmail.com>
2020-06-02 19:48:47 -0400

    Add June Week1 Day2

---
+++ June-2020-30-Day-Code-Challenge/Week1/237_Delete_Node_in_a_Linked_List.java
@@ -0,0 +1,19 @@
+// https://leetcode.com/explore/featured/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3348/
+// No. 237 https://leetcode.com/problems/delete-node-in-a-linked-list/
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+
+class Solution {
+    public void deleteNode(ListNode node) {
+        if (node.next != null) {
+            node.val = node.next.val;
+            node.next = node.next.next;
+        }
+    }
+}
