81cf804c0cf5cf46f15dfda29fc9a0fc2e38b07b
maheshg23 <gmn1994@gmail.com>
2020-06-04 19:31:00 -0400

    Add June Week1 Day4

---
+++ June-2020-30-Day-Code-Challenge/Week1/344_Reverse_String.java
@@ -0,0 +1,14 @@
+// https://leetcode.com/explore/featured/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3350/
+// 344 https://leetcode.com/problems/reverse-string/
+
+class Solution {
+    public void reverseString(char[] s) {
+
+        int l = 0, r = s.length - 1;
+        while (l < r) {
+            char temp = s[l];
+            s[l++] = s[r];
+            s[r--] = temp;
+        }
+    }
+}
