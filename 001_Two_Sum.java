7d6cb1dafe0ec7a1326d898f95d63587d8986666
maheshg23 <gmn1994@gmail.com>
2020-06-03 22:12:52 -0400

    Update 001 $ 002

--- 001_Two_Sum.java
+++ 001_Two_Sum.java
@@ -1,14 +1,14 @@
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        
-        Map<Integer,Integer> hm = new HashMap<>();
-        for(int i = 0; i < nums.length; i++){
+
+        Map<Integer, Integer> hm = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
             int num = target - nums[i];
-            if(hm.containsKey(num)){
-                return new int[] {hm.get(num), i};
+            if (hm.containsKey(num)) {
+                return new int[] { hm.get(num), i };
             }
-            hm.put(nums[i],i);            
+            hm.put(nums[i], i);
         }
-        return new int[] {-1,-1};
+        return new int[] { -1, -1 };
     }
 }
--- 002_Add_Two_Numbers.java
+++ 002_Add_Two_Numbers.java
@@ -1,27 +1,25 @@
 /**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode(int x) { val = x; }
- * }
+ * Definition for singly-linked list. public class ListNode { int val; ListNode
+ * next; ListNode(int x) { val = x; } }
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        int carry=0;
+        int carry = 0;
         ListNode result = new ListNode(0);
         ListNode p = l1, q = l2, curr = result;
-        while(p != null || q != null){
+        while (p != null || q != null) {
             int a = (p != null) ? p.val : 0;
             int b = (q != null) ? q.val : 0;
             int sum = a + b + carry;
             carry = sum / 10;
             curr.next = new ListNode(sum % 10);
             curr = curr.next;
-            if (p != null) p = p.next;
-            if (q != null) q = q.next;
+            if (p != null)
+                p = p.next;
+            if (q != null)
+                q = q.next;
         }
-        if(carry > 0){
+        if (carry > 0) {
             curr.next = new ListNode(carry);
         }
         return result.next;
