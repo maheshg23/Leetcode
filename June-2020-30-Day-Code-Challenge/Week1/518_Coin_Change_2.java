ffc5c47358c2b88255416af67b0f2d45f95d3fb1
maheshg23 <gmn1994@gmail.com>
2020-06-07 21:54:35 -0400

    Add June Week1 Day7

---
+++ June-2020-30-Day-Code-Challenge/Week1/518_Coin_Change_2.java
@@ -0,0 +1,25 @@
+// https://leetcode.com/explore/featured/card/june-leetcoding-challenge/539/week-1-june-1st-june-7th/3353/
+// 518 https://leetcode.com/problems/coin-change-2/
+
+class Solution {
+    public int change(int amount, int[] coins) {
+
+        int[][] dp = new int[coins.length + 1][amount + 1];
+        for (int i = 0; i <= coins.length; i++) {
+            for (int j = 0; j <= amount; j++) {
+                if (j == 0) {
+                    dp[i][j] = 1;
+                } else if (i == 0) {
+                    dp[i][j] = 0;
+                } else {
+                    if (j - coins[i - 1] >= 0) {
+                        dp[i][j] = dp[i][j - coins[i - 1]] + dp[i - 1][j];
+                    } else {
+                        dp[i][j] = dp[i - 1][j];
+                    }
+                }
+            }
+        }
+        return dp[coins.length][amount];
+    }
+}
