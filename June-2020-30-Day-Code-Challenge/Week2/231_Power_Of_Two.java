4bc03b46e4a2097ae00c7b5a6a3bed8bf8aa136c
maheshg23 <gmn1994@gmail.com>
2020-06-09 01:34:47 -0400

    Add June Week2 Day8

---
+++ June-2020-30-Day-Code-Challenge/Week2/231_Power_Of_Two.java
@@ -0,0 +1,16 @@
+// https://leetcode.com/explore/featured/card/june-leetcoding-challenge/540/week-2-june-8th-june-14th/3354/
+// 231 https://leetcode.com/problems/power-of-two/
+
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if (n == 0)
+            return false;
+        while (n != 1) {
+            if (n % 2 == 0)
+                n = n / 2;
+            else
+                return false;
+        }
+        return true;
+    }
+}
