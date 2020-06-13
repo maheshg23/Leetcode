1562e0f82591fbb02ec3d3dcf17dca42f026f921
maheshg23 <gmn1994@gmail.com>
2020-06-13 12:08:29 -0400

    Update April Week1

--- April-2020-30-Day-Code-Challenge/README.md
+++ April-2020-30-Day-Code-Challenge/README.md
@@ -6,10 +6,10 @@ Repository containing my solutions to 30 Days Code Challenge April-2020 on Leetc
 
 - [1-Single Number](Week1/136_Single_Number.java)
 - [2-Happy Number](Week1/202_Happy_Number.java)
-- [3-Maximum SubArray](Week1/53_Maximum_Subarray.java)
+- [3-Maximum SubArray](Week1/053_Maximum_Subarray.java)
 - [4-Move Zeroes](Week1/283_Move_Zeros.java)
 - [5-Best Time to Buy and Sell Stock II](Week1/122_Best_Time_to_Buy_and_Sell_Stock_II.java)
-- [6-Group Anagrams](Week1/49_Group_Anagrams.java)
+- [6-Group Anagrams](Week1/049_Group_Anagrams.java)
 - [7-Counting Elements](Week1/)
 
 ### Week-2
@@ -17,15 +17,15 @@ Repository containing my solutions to 30 Days Code Challenge April-2020 on Leetc
 - [8-Middle of the Linked List](Week2/876_Middle_of_the_Linked_List.java)
 - [9-Backspace String Compare](Week2/844_Backspace_String_Compare.java)
 - [10-Min Stack](Week2/155_Min_Stack.java)
-- [11-Diameter of Binary Tree](Week2/)
-- [12-Last Stone Weight](Week2/)
-- [13-Contiguous Array](Week2/)
+- [11-Diameter of Binary Tree](Week2/543_Diameter_of_Binary_Tree.java)
+- [12-Last Stone Weight](Week2/1046_Last_Stone_Weight.java)
+- [13-Contiguous Array](Week2/525_Contiguous_Array.java)
 - [14-Perform String Shifts](Week2/)
 
 ### Week-3
 
-- [15-Product of Array Except Self](Week3/)
-- [16-Valid Parenthesis String](Week3/)
+- [15-Product of Array Except Self](Week3/238_Product_of_Array_Except_Self.java)
+- [16-Valid Parenthesis String](Week3/678_Valid_Parenthesis_String.java)
 - [17-Number of Islands](Week3/)
 - [18-Minimum Path Sum](Week3/)
 - [19-Search in Rotated Sorted Array](Week3/)
---
+++ April-2020-30-Day-Code-Challenge/Week1/053_Maximum_Subarray.java
@@ -0,0 +1,70 @@
+// https://leetcode.com/explore/challenge/card/30-day-leetcoding-challenge/528/week-1/3285/
+// 53 https://leetcode.com/problems/maximum-subarray/
+
+//Brute Force
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int res = Integer.MIN_VALUE;
+        for (int i = 0; i < nums.length; i++) {
+            int sum = 0;
+            for (int j = 1; j <= nums.length; j++) {
+                if (i + j > nums.length)
+                    break;
+                sum += nums[i + j - 1];
+                if (sum > res)
+                    res = sum;
+            }
+        }
+        return res;
+    }
+}
+
+// Divide and Conquer
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        return subArray(nums, 0, nums.length - 1);
+
+    }
+
+    public int subArray(int[] nums, int l, int r) {
+        if (l == r)
+            return nums[l];
+        int mid = l + (r - l) / 2;
+
+        int leftSub = subArray(nums, l, mid);
+        int rightSub = subArray(nums, mid + 1, r);
+        int singleSum = Math.max(leftSub, rightSub);
+
+        int sum = 0, leftSum = Integer.MIN_VALUE, rightSum = Integer.MIN_VALUE;
+        for (int i = mid + 1; i <= r; i++) {
+            sum += nums[i];
+            if (sum > rightSum)
+                rightSum = sum;
+        }
+        sum = 0;
+        for (int i = mid; i >= l; i--) {
+            sum += nums[i];
+            if (sum > leftSum)
+                leftSum = sum;
+        }
+
+        return Math.max(leftSum + rightSum, singleSum);
+    }
+}
+
+// DP
+class Solution {
+    public int maxSubArray(int[] nums) {
+
+        int max = nums[0];
+        int globalMax = nums[0];
+        for (int i = 1; i < nums.length; i++) {
+            max = Math.max(max + nums[i], nums[i]);
+            globalMax = Math.max(globalMax, max);
+
+        }
+        return globalMax;
+    }
+}
