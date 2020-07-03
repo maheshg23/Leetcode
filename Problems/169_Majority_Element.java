ae1ac02f7efbe31cf1b0f526a56409e36c56207a
maheshg23 <gmn1994@gmail.com>
2020-07-03 19:43:50 -0400

    Update README

--- Problems/169_Majority_Element.java
+++ Problems/169_Majority_Element.java
@@ -7,15 +7,28 @@ class Solution {
         int res = 0;
         int n = nums.length;
         Map<Integer, Integer> hm = new HashMap<Integer, Integer>();
-
         for (int i : nums) {
-
             hm.put(i, hm.get(i) == null ? 1 : hm.get(i) + 1);
-
             if (hm.get(i) > (n / 2)) {
                 res = i;
                 break;
             }
+        }
+        return res;
+    }
+}
+
+class Solution {
+    public int majorityElement(int[] nums) {
+        int res = 0;
+        int c = 0;
+        for (int num : nums) {
+            if (c == 0)
+                res = num;
+            if (num != res)
+                c--;
+            else
+                c++;
         }
         return res;
     }
--- README.md
+++ README.md
@@ -7,3 +7,4 @@ Contains my solutions to various leetcode Problems
 - [April 2020](https://github.com/maheshg23/Leetcode/tree/master/April-2020-30-Day-Code-Challenge "April 2020")
 - [May 2020](https://github.com/maheshg23/Leetcode/tree/master/May-2020-30-Day-Code-Challenge "May 2020")
 - [June 2020](https://github.com/maheshg23/Leetcode/tree/master/June-2020-30-Day-Code-Challenge "June 2020")
+- [July 2020](https://github.com/maheshg23/Leetcode/tree/master/July-2020-30-Day-Code-Challenge "July 2020")
